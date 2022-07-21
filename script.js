f164cae8dd617b81acb5f6d6681c1995f59c229a
kelp of truth <102844552+kelp-of-truth@users.noreply.github.com>
2022-07-21 15:35:48 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const Nav=document.getElementById('nav');
+
+const Header=document.getElementById('_header');
+
+
+Nav.onmouseover=(e)=>{
+    document.querySelectorAll('nav')[0].hidden=false;
+};
+Nav.onmouseout=(e)=>{
+    Nav.querySelectorAll('nav')[0].hidden=true;
+};
+
+
+const mainFrame=document.getElementById('mainFrame');
+mainFrame.style.height=window.innerHeight-80+"px";
+
+document.getElementById('mainPage').onclick=()=>{
+    mainFrame.src="./main.html";
+    localStorage.setItem("page", 0);
+};
+
+document.getElementById('sites').onclick=()=>{
+    mainFrame.src="./sites.html";
+    localStorage.setItem("page", 3);
+};
+
+
+
+
+
+window.onload=()=>{
+
+    // localStorage.removeItem("visited")
+
+    var visited=localStorage.getItem('visited');
+
+    if(visited==null){
+        localStorage.setItem('visited', 1);
+        localStorage.setItem("page", 0);
+        mainFrame.src="./main.html";
+    }else{
+        var page=localStorage.getItem("page");
+        if(page==0){
+            mainFrame.src="./main.html";
+        }else
+        if(page==3){
+            mainFrame.src="./sites.html";
+        }
+    }
+
+};
