a65f16840bffa7fff79439c5c9c58ae1d9d46be4
kelp of truth <102844552+kelp-of-truth@users.noreply.github.com>
2022-09-24 16:38:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // $(".main").css("height", window.innerHeight+"px");
 // $(".main").css("width", window.innerWidth+"px");
-if(window.innerHeight>=1000){
-   $("body").css("fontSize", "0.5%");
+if(window.innerWidth>=1000){
+   $("body").css("font-size", "0.5%");
 };
