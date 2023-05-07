ed61ca53e17cb0b932fd59894bf16ac3ce6c55e3
kelp of truth <102844552+kelp-of-truth@users.noreply.github.com>
2023-05-08 08:17:46 +0900

    Update script.js

--- tweetBtn/script.js
+++ tweetBtn/script.js
@@ -5,7 +5,7 @@ $("#tweetBtn").on("click",()=>{
 
     var link=String($("#content").val());
 
-    if(link.search(/https:\/\/www.youtube.com\/watch/g)!==-1||link.search(/https:\/\/youtube.com\/watch/g)!==-1){
+    if(link.search(/https:\/\/www.youtube.com\/watch/g)!==-1||link.search(/https:\/\/youtube.com\/watch/g)!==-1||link.search(/https:\/\/m.youtube.com\/watch/g)||||link.search(/https:\/\/www.m.youtube.com\/watch/g)){
         var youtubeURL=new URL(link);
         var params=new URLSearchParams(youtubeURL.search);
         var url;
@@ -43,7 +43,28 @@ $("#tweetBtn").on("click",()=>{
 
 
 
-    }else{
+    }else if(link.search(/https:\/\/youtu.be/g)!==-1||link.search(/youtu.be/g)!==-1){
+        youtubeID=link.replace(/https:\/\//g,"").split("/")[1];
+        
+        fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics,status&id=${youtubeID}&key=AIzaSyCYXe_ZDaVTG31H9ZVjHa_5auz_NWqeKYI`)
+            .then((res)=>{
+                return res.json();
+            })
+            .then((data)=>{
+                // console.log(data)
+                // console.log(data.items[0].statistics.likeCount)
+                url=`https:\/\/twitter.com\/intent\/tweet?url=${link}&text=視聴回数：${fixNum(Number(data.items[0].statistics.viewCount))}回%0D%0A👍高評価：${fixNum(Number(data.items[0].statistics.likeCount))}%0D%0A💬コメント：${data.items[0].statistics.commentCount}件%0D%0A投稿日：${String(data.items[0].snippet.publishedAt).replace(/-/g,"/").replace(/T/g,"　").replace(/Z/g,"")} %0D%0A%0D%0Aデータ取得時刻：${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}　${date.getHours()}:${date.getMinutes()}`;
+                window.location.href=String(url);
+                // console.log(url)
+                // console.log(apiData)
+                // console.log(apiData.items[0].snippet.title)
+            })
+            .catch((err)=>{
+                console.log(err);
+                alert("URLが正しくありません");
+            });
+    }
+    else{
         alert("URLが正しくありません");
     }
 })
